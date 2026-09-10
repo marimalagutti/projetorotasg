934c3d2545fba4c56b5cda3060e97e9a2c73c5ec
Mariana <marianamalagutti>
2026-09-10 11:31:52 -0300

    feat: codificando os components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Contato from './pages/Contato'
+import Produto from './pages/Produto'
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <Router>
+      <div className="min-h-screen bg-blue-50 text-gray-900">
+        <NavBar/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sobre" element={<Sobre/>}/>
+            <Route path="/contato" element={<Contato/>}/>
+            <Route path="/produto" element={<Produto/>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <>
+    <nav className="bg-blue-400 shadow-md">
+      <div>
+        <span>Projeto</span>
+      </div>
+
+      <div className="flex space-x-4 items-center">
+        <Link to="/" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Home</Link>
+        <Link to="/sobre" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600" >Sobre</Link>
+        <Link to="/produto" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Produto</Link>
+        <Link to="/contato" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Contato</Link>
+      </div>
       
-    </>
+    </nav>
   )
 }
 
