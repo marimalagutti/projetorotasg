bdd95901953ad402d581d7f4119befd4a33e077f
Mariana <marianamalagutti>
2026-09-10 11:40:18 -0300

    feat: adicionando icones no projeto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import {Link} from 'react-router-dom'
+import {Home, Info} from 'lucide-react'
 
 const NavBar = () => {
   return (
     <nav className="bg-blue-400 shadow-md">
-      <div>
-        <span>Projeto</span>
+      <div className="max-w-5xl mx-auto px-4">
+        <span className="text-xl font-bold text-white">Projeto</span>
       </div>
 
       <div className="flex space-x-4 items-center">
-        <Link to="/" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Home</Link>
-        <Link to="/sobre" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600" >Sobre</Link>
+        <Link to="/" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600"><Home/>Home</Link>
+        <Link to="/sobre" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600" ><Info/>Sobre</Link>
         <Link to="/produto" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Produto</Link>
         <Link to="/contato" className="flex items-center px-3 py-2 font-medium text-amber-200 hover:text-amber-600">Contato</Link>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <h1>Bem-Vindo a Página<span>Home</span></h1>
+      <h1>Bem-Vindo a Página <span>Home</span></h1>
     </>
   )
 }
